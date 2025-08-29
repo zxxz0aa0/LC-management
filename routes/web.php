ce4119bb8b3ee7c5a68117458c93450037480bfc
zxxz0aa0 <raymon-cl@hotmail.com>
2025-08-29 16:13:17 +0800

    調整網址登入導向

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect()->route('dashboard') : redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
